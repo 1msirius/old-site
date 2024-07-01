90790e5d0eeeba32c6a5f7ac74b876d1b103b004
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-07-01 23:15:30 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,63 +1,14 @@
-import { FaXTwitter } from 'react-icons/fa6'
-import { FaGithub, FaLinkedin, FaGoodreadsG, FaSpotify } from 'react-icons/fa'
-import { TbMailFilled } from 'react-icons/tb'
-const YEAR = new Date().getFullYear()
+import React from 'react';
+import Footer from './components/footer';
+
+const Head = ({ title }) => (
+  <title>
+    {title && title !== 'About' ? `${title} | Sirius` : 'Sirius'}
+  </title>
+);
 
 export default {
   darkMode: true,
-  head: ({ title }) => (
-    <>
-      <title>
-        {title
-          ? title !== 'About'
-            ? title + ' | Sirius'
-            : 'Sirius'
-          : 'Sirius'}
-      </title>
-    </>
-  ),
-  footer: (
-    <small className="block mt-32">
-      <time>© {YEAR} </time>{' '}
-      <a
-        className="no-underline"
-        href="https://x.com/1msirius_"
-        target="_blank"
-      >
-        {' '}
-        Sirius.
-      </a>
-      <style jsx>{`
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
-      <div className="flex text-lg gap-3.5 float-right">
-        <a href="https://x.com/1msirius_" target="_blank">
-          <FaXTwitter />
-        </a>
-        <a href="https://github.com/1msirius" target="_blank">
-          <FaGithub />
-        </a>
-        <a href="https://www.goodreads.com/akshattalapa" target="_blank">
-          <FaGoodreadsG />
-        </a>
-        <a
-          href="https://open.spotify.com/user/31cscbv4s256ppqweoixud5hxq6q?si=135e0066f0574c33"
-          target="_blank"
-        >
-          <FaSpotify />
-        </a>
-        <a href="https://www.linkedin.com/in/akshattalapa/" target="_blank">
-          <FaLinkedin />
-        </a>
-        <a href="mailto:akshattalapa27@gmail.com" target="_blank">
-          <TbMailFilled />
-        </a>
-      </div>
-    </small>
-  )
-}
+  head: Head,
+  footer: <Footer />
+};
