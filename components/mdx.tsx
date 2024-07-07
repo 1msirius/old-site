422e5b1d795a9e3e9c61fc94c90ff77adc2a77b8
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-07-07 21:25:24 +0530

    Add mdx component

---
+++ components/mdx.tsx
@@ -0,0 +1,11 @@
+import { Caption } from './caption'
+import { YouTube } from './youtube'
+import { TweetComponent } from './tweet';
+
+export function MDXComponents() {
+    return {
+      Caption,
+      YouTube,
+      TweetComponent,
+    };
+  }
